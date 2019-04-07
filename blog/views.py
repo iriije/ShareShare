bdd3516e6158c6ef77add5814625dccbd9535a72
JunkYoung <wnsrud3611@gmail.com>
2019-04-07 22:34:46 +0900

    bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+from django.http import HttpResponse
+from django.template import loader
+  
+def index(request):
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_question_list': "test",
+    }
+    return HttpResponse(template.render(context, request))
+
 	
