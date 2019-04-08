51cb5c4987baaf9a5de87ba03a17c573d5662bcc
JunkYoung <wnsrud3611@gmail.com>
2019-04-08 15:42:31 +0900

    login function

---
+++ member/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as django_login, logout as django_logout, authenticate
+from .forms import LoginForm, SignupForm
+
+
+def login(request):
+    if request.method == 'POST':
+        next = request.GET.get('next')
+        login_form = LoginForm(request=request, data=request.POST)
+
+        if login_form.is_valid():
+            user = login_form.get_user()
+            django_login(request, user)
+            return redirect(next if next else 'index')
+        login_form.add_error(None, '아이디 또는 비밀번호가 올바르지 않습니다')
+    else:
+        login_form = LoginForm()
+
+    context = {
+        'login_form': login_form,
+    }
+    return render(request, 'member/login.html', context)
+
+def logout(request):
+    django_logout(request)
+    return redirect('index')
+
+
+def signup(request):
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            django_login(request, user)
+            return redirect('index')
+    else:
+        signup_form = SignupForm()
+
+    context = {
+        'signup_form': signup_form,
+    }
+    return render(request, 'member/signup.html', context)
--- shareshare/urls.py
+++ shareshare/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path(r'^member/', include('member.urls', namespace='member')),
 ]
